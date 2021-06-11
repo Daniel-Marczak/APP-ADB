b407a7136cbae435ed69170c4918163933fe7dce
Daniel-Marczak <daniel.t.marczak@gmail.com>
2021-06-11 13:27:19 +0200

    Moved SecurityConfig to config

--- src/main/java/pl/danielmarczak/adb/config/SecurityConfig.java
+++ src/main/java/pl/danielmarczak/adb/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package pl.danielmarczak.adb.security;
+package pl.danielmarczak.adb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
