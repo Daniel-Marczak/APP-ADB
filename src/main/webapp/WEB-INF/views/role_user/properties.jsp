300ffed0f2d00c414ff96ed43f9f69c44676cffa
Daniel-Marczak <daniel.t.marczak@gmail.com>
2021-06-25 23:58:08 +0200

    USER'S PROPERTIES - DISPLAY
    app-role-user-properties.js - modified 'getAllPropertiesByUserId' method (external functions are called inside the method to create DOM elements). Added functions: 'convertPropertyPhotoFileDataToBlob', 'displayPropertyCard', 'createPropertyNameTabElement', 'createPropertyCardElement', 'createPropertyDetailsElement', 'createPropertyPhotoElement', 'createPropertyAddressElement', 'createPropertyDescriptionElement', 'createPropertyRoomsElement'.
    camera_icon.png - planning to use it for displaying a propertyPhotoElement id a user doesn't submit his/her own photo.
    properties.jsp - changed <div class="portfolio"> to <div class="banner">
    style.css - added few classes for styling properties elements

--- src/main/webapp/WEB-INF/views/role_user/properties.jsp
+++ src/main/webapp/WEB-INF/views/role_user/properties.jsp
@@ -14,59 +14,58 @@
 <%@include file="/WEB-INF/views/jspf/user-navbar.jspf" %>
 
 
-<!-- Portfolio -->
-<div id="a" class="portfolio">
-    <div class="contact" id="contact">
-        <div class="container" style="">
-            <div class="row" style="height: 800px">
-                <div class="col-md-12">
-                    <div class="text-center">
-                        <input type="hidden" name="userId" value="${requestScope.userId}">
-                        <h3>Properties</h3>
-                        <div class="properties-container"></div>
-                    </div>
+<!-- Banner -->
+<div class="banner" id="banner">
+    <div class="bg-overlay" style="padding: 70px 250px 100px 250px">
+        <div class="container-xl" style="height: 80%; margin-top: 60px">
+            <input type="hidden" name="userId" value="${requestScope.userId}">
+<%--            <h3 style="padding-bottom: 20px">Properties</h3>--%>
+            <div class="properties-container">
+                <div class="property-name-tab-container">
                 </div>
-            </div>
-            <div class="date-picker"></div>
-        </div>
-    </div>
-
-    <!-- FOOTER -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4"><span class="copyright">Copyright &copy; Ethereal 2018</span></div>
-                <div class="col-md-4">
-                    <ul class="list-inline social-buttons">
-                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <ul class="list-inline quicklinks">
-                        <li>Designed by <a href="<c:url value="https://w3template.com"/>">W3 Template</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-    <!-- LOGOUT MODAL -->
-    <div tabindex="-1" class="modal bs-example-modal-sm" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-sm">
-            <div class="modal-content">
-                <div class="modal-header"><h4>Logout <i class="fa fa-lock"></i></h4></div>
-                <div class="modal-body"><i class="fa fa-question-circle"></i> Are you sure you want to log-off?</div>
-                <div class="modal-footer">
-                    <form action="<c:url value="/logout"/> " method="post">
-                        <button type="submit" class="btn btn-primary btn-block">Logout</button>
-                    </form>
+                <div class="property-card-container">
                 </div>
             </div>
         </div>
     </div>
-    <%@include file="/WEB-INF/views/jspf/footer.jspf" %>
-    <script src="<c:url value="/resources/js/app-role-user-properties.js"/> "></script>
+</div>
+
+<!-- FOOTER -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"><span class="copyright">Copyright &copy; Ethereal 2018</span></div>
+            <div class="col-md-4">
+                <ul class="list-inline social-buttons">
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <ul class="list-inline quicklinks">
+                    <li>Designed by <a href="<c:url value="https://w3template.com"/>">W3 Template</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- LOGOUT MODAL -->
+<div tabindex="-1" class="modal bs-example-modal-sm" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header"><h4>Logout <i class="fa fa-lock"></i></h4></div>
+            <div class="modal-body"><i class="fa fa-question-circle"></i> Are you sure you want to log-off?</div>
+            <div class="modal-footer">
+                <form action="<c:url value="/logout"/> " method="post">
+                    <button type="submit" class="btn btn-primary btn-block">Logout</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<%@include file="/WEB-INF/views/jspf/footer.jspf" %>
+<script src="<c:url value="/resources/js/app-role-user-properties.js"/> "></script>
 </body>
 </html>
--- src/main/webapp/resources/js/app-role-user-properties.js
+++ src/main/webapp/resources/js/app-role-user-properties.js
@@ -1,10 +1,25 @@
+const propertiesContainer = $('.properties-container');
+let propertyNameElementArray = [];
+let propertyCardElementArray = [];
+let propertyDetailsElementArray = [];
+let propertyPhotoElementArray = [];
+
+
+document.addEventListener("contextmenu", function () {
+    console.log('propertyDivsArray => ')
+    console.log()
+    console.log('---------------------------------------------------------')
+    console.log('propertyDivsArray =>')
+    console.log()
+});
+
+
 function getAllPropertiesByUserId() {
     const userId = $('input[type=hidden]').val();
-    return $.get(`http://localhost:8080/api/property/user-properties/${userId}`, function (properties, status) {
-
-        console.log(status);
-
+    let propertyCounter = 0;
+    $.get(`http://localhost:8080/api/property/user-properties/${userId}`, function (properties) {
         properties.forEach((property => {
+            // console.log(property);
 
             const {
                 id,
@@ -24,29 +39,97 @@ function getAllPropertiesByUserId() {
             const {propertyTypeName} = propertyType;
             const {contactNumber, email, username} = user;
 
-            const propertiesContainer = $('.properties-container');
-            const propertyDiv = $('<div class="property-div"></div>');
-            const propertyNameDiv = $('<div class="property-name-div"></div>');
-            const propertyNameSpan = $('<div class="property-name-span"></div>').text(propertyName);
-            const propertyNoPhotoDiv = $('<div class="property-no-photo-div">');
-            const propertyPhotoImg =  $('<img alt="" src="" class="property-photo-img">');
+            let propertyIdentifier = `property-${propertyCounter}`;
 
-            const byteCharacters = atob(fileData);
-            const byteNumbers = new Array(byteCharacters.length);
-            for (let i = 0; i < byteCharacters.length; i++) {
-                byteNumbers[i] = byteCharacters.charCodeAt(i);
-            }
-            const byteArray = new Uint8Array(byteNumbers);
-            const blob = new Blob([byteArray], {type: "image/jpg"});
-            propertyPhotoImg.attr('src', URL.createObjectURL(blob)).attr('alt', fileName);
+            createPropertyNameTabElement(propertyCounter, propertyIdentifier, propertyName);
+            createPropertyCardElement(propertyCounter, propertyIdentifier);
+            createPropertyDetailsElement(propertyCounter, propertyIdentifier);
+            createPropertyPhotoElement(propertyCounter, propertyIdentifier, fileData, fileName);
 
-            propertiesContainer.append(propertyDiv.append(propertyNameDiv.append(propertyNameSpan)).append(propertyPhotoImg));
 
+            // console.log(propertyCardElementArray);
+            propertyCounter++
         }))
     });
 }
 
 getAllPropertiesByUserId();
+
+function convertPropertyPhotoFileDataToBlob(fileData) {
+    const byteCharacters = atob(fileData);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    return new Blob([byteArray], {type: "image/jpg"})
+}
+
+function displayPropertyCard() {
+    propertyCardElementArray.forEach(card => {
+        $(card).hasClass(`${$(this.classList)[1]}`) ? $(card).removeClass('hidden').toggle().fadeIn(300) : $(card).addClass('hidden').toggle().fadeOut(300);
+    });
+}
+
+function createPropertyNameTabElement(propertyCounter, propertyIdentifier, propertyName) {
+    const propertyNameTabContainer = $('.property-name-tab-container');
+    const propertyNameTabElement = $('<div class="property-name-tab"></div>');
+    const propertyNameTextElement = $('<h3 style="margin-top: 0px"></h3>').text(propertyName);
+    propertyNameTabElement.addClass(propertyIdentifier);
+    propertyNameTabElement.on('click', displayPropertyCard);
+    propertyNameTabContainer.append(propertyNameTabElement.append(propertyNameTextElement));
+    propertyNameElementArray.push(propertyNameTabElement[0].context);
+}
+
+function createPropertyCardElement(propertyCounter, propertyIdentifier) {
+    const propertyCardContainer = $('.property-card-container');
+    const propertyCardElement = $('<div class="property-card"></div>');
+    propertyCardElement.addClass(propertyIdentifier);
+    if (propertyCounter !== 0) {
+        propertyCardElement.addClass('hidden');
+    }
+    propertyCardContainer.prepend(propertyCardElement);
+    propertyCardElementArray.push($(propertyCardElement[0]).context);
+}
+
+function createPropertyDetailsElement(propertyCounter, propertyIdentifier){
+    const propertyCardElement = $(propertyCardElementArray[propertyCounter])
+    const propertyDetailsElement = $('<div class="property-details"></div>');
+    propertyDetailsElement.addClass(propertyIdentifier);
+    propertyCardElement.append(propertyDetailsElement);
+    propertyDetailsElementArray.push($(propertyDetailsElement[0]).context);
+}
+
+function createPropertyPhotoElement(propertyCounter, propertyIdentifier, fileData, fileName) {
+    const propertyDetailsElement = $(propertyDetailsElementArray[propertyCounter]);
+    const propertyPhotoElement = $('<img alt="" src="" class="property-photo">');
+    const imgSrc = convertPropertyPhotoFileDataToBlob(fileData);
+    propertyPhotoElement.attr('src', URL.createObjectURL(imgSrc)).attr('alt', fileName);
+    propertyPhotoElement.addClass(propertyIdentifier);
+    propertyDetailsElement.append(propertyPhotoElement);
+    propertyPhotoElementArray.push($(propertyPhotoElement[0]));
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+function createPropertyAddressElement(propertyCounter,propertyIdentifier, propertyAddressId, city, country, postalCode, province, region, street) {
+    const propertyDetailsElement = $(propertyDetailsElementArray[propertyCounter]);
+}
+
+
+
+
+function createPropertyDescriptionElement(propertyDescriptionId, propertyDescription) {
+
+}
+
+function createPropertyRoomsElement() {
+
+}
+
 
 
 
