4818d1a0a4cd1d9674b1b30a09d8736f32f4f471
Daniel-Marczak <daniel.t.marczak@gmail.com>
2021-06-30 18:06:17 +0200

    FULL CALENDAR JAVA CLASSES (2)
    PropertyCalendarRepository.java - changed JpaRepository generic from 'PropertyCalendarRepository' to 'PropertyCalendar', which, obviously, got there by accident.

--- src/main/java/pl/danielmarczak/adb/repository/PropertyCalendarRepository.java
+++ src/main/java/pl/danielmarczak/adb/repository/PropertyCalendarRepository.java
@@ -2,7 +2,8 @@ package pl.danielmarczak.adb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.danielmarczak.adb.entity.PropertyCalendar;
 
 @Repository
-public interface PropertyCalendarRepository extends JpaRepository<PropertyCalendarRepository, Long> {
+public interface PropertyCalendarRepository extends JpaRepository<PropertyCalendar, Long> {
 }
